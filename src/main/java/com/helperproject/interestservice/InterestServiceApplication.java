86292a321e73fdcacaf8a970a8c3d7cbf8dc10b2
AerenN <aerenbaysal@gmail.com>
2022-02-19 14:44:04 +0300

    First Commit initialzr

---
+++ src/main/java/com/helperproject/interestservice/InterestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.helperproject.interestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helperproject/interestservice/InterestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helperproject.interestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
